40f0e8e124ae9eaf0a9ccc883e35462539563521
hym <hans@kisti.re.kr>
2023-12-09 14:54:16 +0900

    Edit exp4

--- gentcr/interpret.py
+++ gentcr/interpret.py
@@ -4,7 +4,7 @@ import captum.insights.attr_vis.server
 import torch.cuda
 import torch.nn.functional as F
 import unittest
-from captum.attr import configure_interpretable_embedding_layer, LayerConductance
+from captum.attr import configure_interpretable_embedding_layer, LayerConductance, LayerIntegratedGradients
 from peft import AutoPeftModel
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
@@ -30,16 +30,40 @@ class EsmMLMAttrInterpreter:
         clone.zero_grad()
         return clone
 
-    def layer_integrated_gradients_attrs(self, input_seqs, target_pos=0):
-        pass
+    def layer_integrated_gradients_attrs(self, inputs, bl_input_ids=None):
+        def forward_func(input_ids, model, attention_mask=None, target_mask=None):
+            # inputs.shape = (batch_size, max_len, hidden_size)
+            # shape of input_ids, attention_mask, target_mask = (batch_size, max_len)
+            output = model(input_ids=input_ids, attention_mask=attention_mask)
+            # output.logits.shape = (batch_size, max_len, vocab_size)
+            logits = output.logits[target_mask] # logits.view(-1, vocab_size)
+            logits = F.softmax(logits, dim=-1)
+            return logits
+
+        model = self.clone_model()
+        inputs = inputs.to(self.device)
+        bl_input_ids = bl_input_ids.to(self.device)
+        targets = inputs['labels']
+        target_mask = (targets != -100)
+        targets = targets[target_mask]
+        lig = LayerIntegratedGradients(forward_func, model.esm.embeddings)
+        attrs = lig.attribute(inputs=inputs['input_ids'],
+                              baselines=bl_input_ids,
+                              internal_batch_size=8,
+                              additional_forward_args=(model, inputs['attention_mask'], target_mask),
+                              target=targets,
+                              return_convergence_delta=False)
+        return attrs
+
 
     def layer_conductance_attrs(self, inputs, bl_input_ids=None):
-        def lc_attr_forward(inputs, model, input_ids=None, attention_mask=None):
+        def forward_func(inputs, model, input_ids=None, attention_mask=None, target_mask=None):
+            # inputs.shape = (batch_size, max_len, hidden_size)
+            # shape of input_ids, attention_mask, target_mask = (batch_size, max_len)
             output = model(inputs_embeds=inputs, attention_mask=attention_mask)
             # output.logits.shape = (batch_size, max_len, vocab_size)
-            logits = output.logits[input_ids == model.config.mask_token_id]
-            logits = F.softmax(logits.view(-1, model.config.vocab_size), dim=-1)
-            # logits = output.logits.view(-1, model.config.vocab_size)
+            logits = output.logits[target_mask] # logits.view(-1, vocab_size)
+            logits = F.softmax(logits, dim=-1)
             return logits
 
         model = self.clone_model()
@@ -48,7 +72,8 @@ class EsmMLMAttrInterpreter:
 
         input_ids = inputs["input_ids"]
         targets = inputs['labels']
-        targets = targets[input_ids == model.config.mask_token_id]
+        target_mask = (targets != -100)
+        targets = targets[target_mask]
         interpretable_embedding = configure_interpretable_embedding_layer(model,
                                                                           'esm.embeddings.word_embeddings')
         input_embeddings = interpretable_embedding.indices_to_embeddings(input_ids)
@@ -57,13 +82,16 @@ class EsmMLMAttrInterpreter:
         layer_attrs = []
         layer_attns = []
         for i, layer in enumerate(model.esm.encoder.layer):
-            lc = LayerConductance(lc_attr_forward, layer)
+            lc = LayerConductance(forward_func, layer)
             # attrs.shape = (batch_size, max_len, hidden_size), attns.shape = (batch_size, num_heads, max_len, max_len)
             attrs, attns = lc.attribute(inputs=input_embeddings,
                                         baselines=bl_input_embeddings,
                                         internal_batch_size=8,
                                         # n_steps=40,
-                                        additional_forward_args=(model, inputs['input_ids'], inputs['attention_mask']),
+                                        additional_forward_args=(model,
+                                                                 inputs['input_ids'],
+                                                                 inputs['attention_mask'],
+                                                                 target_mask),
                                         target=targets.view(-1))
             layer_attrs.append(attrs)
             layer_attns.append(attns)
@@ -80,15 +108,13 @@ class EsmMLMAttrInterpreterTest(unittest.TestCase):
         self.model, self.tokenizer = self.load_model_and_tokenizer(self.mlm_name_or_path)
         self.interpreter = EsmMLMAttrInterpreter(model=self.model)
         self.eval_ds = EpitopeTargetDataset.from_key('shomuradova_minervina_gfeller')
-        epitope_seq_mutator = FixedPosAASeqMutator(mut_positions=[3], mut_probs=[0.7, 0.3])
+        epitope_seq_mutator = FixedPosAASeqMutator(mut_positions=[3], mut_probs=[0.6, 0.4])
 
-        self.seq_format = '{epitope_seq}{target_seq}'
         self.collator = EpitopeTargetMaskedLMCollator(tokenizer=self.tokenizer,
                                                      epitope_seq_mutator=epitope_seq_mutator,
                                                      target_seq_mutator=None,
                                                      max_epitope_len=self.eval_ds.max_epitope_len,
-                                                     max_target_len=self.eval_ds.max_target_len,
-                                                     seq_format=self.seq_format)
+                                                     max_target_len=self.eval_ds.max_target_len)
 
     def first_batch(self, batch_size=1):
         return next(iter(DataLoader(self.eval_ds, batch_size=batch_size, shuffle=False, collate_fn=self.collator)))
@@ -105,8 +131,10 @@ class EsmMLMAttrInterpreterTest(unittest.TestCase):
         return bl_input_ids
 
     def test_layer_conductance_attrs(self):
-        self.collator.epitope_seq_mutator.mut_positions = [3, 5]
+        self.collator.epitope_seq_mutator.mut_positions = [3]
         inputs = self.first_batch(batch_size=1)
+        input_ids = inputs['input_ids']
+        print(f'Masked input[0]: {self.tokenizer.decode(input_ids[0])}')
         bl_input_ids = self.get_baseline_input_ids(inputs['input_ids'])
         attrs, attns = self.interpreter.layer_conductance_attrs(inputs, bl_input_ids=bl_input_ids)
         # attrs.shape = (num_layers, batch_size, max_len, hidden_size)
@@ -115,7 +143,6 @@ class EsmMLMAttrInterpreterTest(unittest.TestCase):
         attns = TorchUtils.to_numpy(attns)
         print(attrs.shape)
         print(attns.shape)
-        # attrs = reduce_attrs(attrs)
         attrs = np.sum(attrs, axis=(0, 3))
         attrs = attrs / np.linalg.norm(attrs)
         # attrs = np.mean(attrs, axis=(0, 3))
@@ -123,14 +150,12 @@ class EsmMLMAttrInterpreterTest(unittest.TestCase):
         attns = attns / np.linalg.norm(attns)
         # attns = np.mean(attns, axis=(0, 2, 3))
 
-        # tokens = [self.tokenizer.cls_token] + list(self.input_seqs[0]) + [self.tokenizer.eos_token]
-        seq = list(self.seq_format.format(epitope_seq=self.eval_ds.df.epitope_seq[0],
-                                          target_seq=self.eval_ds.df.cdr3b_seq[0]))
-        tokens = [self.tokenizer.cls_token] + seq + [self.tokenizer.eos_token]
-        # tokens = self.tokenizer.convert_ids_to_tokens(inputs['input_ids'][0])
+        seq = self.collator.format_seqs(self.eval_ds.df.epitope_seq[0],
+                                        self.eval_ds.df.cdr3b_seq[0])
+        tokens = [self.tokenizer.cls_token] + list(seq) + [self.tokenizer.eos_token]
+
         fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(7, 12))
         plt.tight_layout()
-
         ax = sns.barplot(attrs[0][:len(tokens)], ax=axes[0])
         ax.set_xticks(range(len(tokens)))
         ax.set_xticklabels(tokens)
@@ -139,7 +164,30 @@ class EsmMLMAttrInterpreterTest(unittest.TestCase):
         ax = sns.barplot(attns[0][:len(tokens)], ax=axes[1])
         ax.set_xticks(range(len(tokens)))
         ax.set_xticklabels(tokens)
+        plt.show()
 
+    def test_layer_integrated_grad_attrs(self):
+        self.collator.epitope_seq_mutator.mut_positions = [3, 5]
+        inputs = self.first_batch(batch_size=1)
+        input_ids = inputs['input_ids']
+        print(f'Masked input[0]: {self.tokenizer.decode(input_ids[0])}')
+        bl_input_ids = self.get_baseline_input_ids(inputs['input_ids'])
+        attrs, delta = self.interpreter.layer_integrated_gradients_attrs(inputs, bl_input_ids=bl_input_ids)
+        # attrs.shape = (batch_size, max_len, hidden_size)
+        attrs = TorchUtils.to_numpy(attrs)
+        print(attrs.shape)
+        attrs = np.sum(attrs, axis=-1)
+        attrs = attrs / np.linalg.norm(attrs)
+        # attrs = np.mean(attrs, axis=-1)
+        seq = self.collator.format_seqs(self.eval_ds.df.epitope_seq[0],
+                                        self.eval_ds.df.cdr3b_seq[0])
+        tokens = [self.tokenizer.cls_token] + list(seq) + [self.tokenizer.eos_token]
+
+        fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(7, 12))
+        plt.tight_layout()
+        ax = sns.barplot(attrs[0][:len(tokens)], ax=axes[0])
+        ax.set_xticks(range(len(tokens)))
+        ax.set_xticklabels(tokens)
         plt.show()
 
 if __name__ == '__main__':
